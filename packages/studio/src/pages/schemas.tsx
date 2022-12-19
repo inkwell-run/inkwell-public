83aeb3a8e0e8c97a30d068de62161c3efb7fdd40
Sarim Abbas <sarim.stays@gmail.com>
2022-12-19 22:47:45 +0500

    wip

--- packages/studio/src/pages/schemas.tsx
+++ packages/studio/src/pages/schemas.tsx
@@ -2,13 +2,14 @@ import { useAtomValue } from "jotai";
 import React from "react";
 import { ISchema } from "./_app";
 import { GlobalStateAtom } from "../lib/store";
+import { Stack } from "@manuscript/lib";
 
 const SchemasPage = () => {
   const { baseProps } = useAtomValue(GlobalStateAtom);
   const { schemas } = baseProps;
 
   return (
-    <div className="flex flex-col w-full gap-4 p-4">
+    <Stack sx={{ gap: "sm", padding: "sm" }}>
       <h1 className="text-lg">Schemas</h1>
       <p>These are all the schemas you have defined</p>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
@@ -16,7 +17,7 @@ const SchemasPage = () => {
           return <SchemaDisplay schema={s} key={`${i}-${s.name}`} />;
         })}
       </div>
-    </div>
+    </Stack>
   );
 };
 
