466118c7987285a60a0bce98c58d72d9b291ab76
Sarim Abbas <sarim.stays@gmail.com>
2023-02-16 22:48:12 -0800

    remove nextjs

--- packages/studio-nextjs/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-    transpilePackages: ["@doom.sh/ui"],
-  },
-};
-
-module.exports = nextConfig;
