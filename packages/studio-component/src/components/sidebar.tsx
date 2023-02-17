05a18dabc7b88a03a452c770cf772d4ce86cd110
Sarim Abbas <sarim.stays@gmail.com>
2023-02-17 00:05:50 -0800

    cleanup

--- packages/studio-component/src/components/sidebar.tsx
+++ packages/studio-component/src/components/sidebar.tsx
@@ -15,33 +15,8 @@ const Sidebar = () => {
           <NavLink label="Schemas" />
         </Link>
       </Navbar.Section>
-      <Navbar.Section>
-        <ConnectionStatus />
-      </Navbar.Section>
     </Stack>
   );
 };
 
 export default Sidebar;
-
-const ConnectionStatus = () => {
-  const accessToken = useQuery({
-    queryKey: ["access-token"],
-    queryFn: ManuscriptApi.AccessTokensService.queryAccessTokensTest,
-  });
-
-  const organization = useQuery({
-    queryKey: ["organization", accessToken.data?.organizationId!],
-    queryFn: () =>
-      ManuscriptApi.OrganizationsService.queryOrganizationsFindUnique(
-        accessToken.data?.organizationId!
-      ),
-    enabled: !!accessToken.data?.organizationId,
-  });
-
-  return (
-    <Alert title="Connected to:" color="lime" radius="md">
-      {organization.data?.clerkOrganizationName ?? <>&nbsp;</>}
-    </Alert>
-  );
-};
