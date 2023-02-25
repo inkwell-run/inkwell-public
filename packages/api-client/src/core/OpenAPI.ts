a40fe71be3f47dd4738b960c9cd63486ca164e12
Sarim Abbas <sarim.stays@gmail.com>
2023-02-24 23:28:49 -0800

    regen

--- packages/api-client/src/core/OpenAPI.ts
+++ packages/api-client/src/core/OpenAPI.ts
@@ -19,7 +19,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'https://dashboard.manuscriptcms.com/api',
+    BASE: 'https://app.inkwell.run/api',
     VERSION: '1.0.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
