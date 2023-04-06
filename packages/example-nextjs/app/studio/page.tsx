11809059b0cab7cae2c9ea1a416e151dfe0d6ce1
Sarim Abbas <sarim.stays@gmail.com>
2023-04-05 22:41:30 -0700

    wip

--- packages/example-nextjs/app/studio/page.tsx
+++ packages/example-nextjs/app/studio/page.tsx
@@ -5,11 +5,7 @@ import { InkwellStudio } from "@inkwell.run/studio";
 import "@inkwell.run/studio/dist/index.css";
 
 const Studio = () => {
-  return (
-    <div>
-      <InkwellStudio accessToken="20d73831-4255-46ff-8021-aaadab975554" />
-    </div>
-  );
+  return <InkwellStudio accessToken="20d73831-4255-46ff-8021-aaadab975554" />;
 };
 
 export default Studio;
